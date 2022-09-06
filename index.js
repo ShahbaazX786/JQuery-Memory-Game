13d0d00144e687c7b10031ff8abfa942170d08c0
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-06 16:45:53 +0530

    CL-1.05-Added function to play sound fx

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ function randum(){
     var randomnumber=Math.floor(Math.random()*4);
     var randomcolor=buttoncolors[randomnumber];
     gamepattern.push(randomcolor);
-    // console.log(gamepattern);
+    playsound(randomcolor);
+    console.log(gamepattern);
 }
 
+// $("#randomcolor")
+
+// // function anime(randomcolor){
+// //     $("#"+randomcolor).fadeIn(100).fadeOut(100);
+// // }
+
+function playsound(randomcolor){
+    var sfx = new Audio('sounds/'+randomcolor+".mp3");
+    sfx.play();
+}
