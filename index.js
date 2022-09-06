62482a73788a6952cd6fff27950ca4ddc62d629b
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-06 17:30:57 +0530

    CL-1.07-Added level changes to h1 and functions.

--- index.js
+++ index.js
@@ -4,21 +4,38 @@ gamepattern=[];
 
 userClickedPattern=[];
 
+var level=0;
+
+var started=false;
+
+$(document).keydown(function(){
+    if(!started){
+        $("h1").text("Level "+level);
+        nextSequence();
+        started=true;
+    }
+});
+
 
 $(".btn").click(function (){
     var userclickedcolor=$(this).attr("id");
     userClickedPattern.push(userclickedcolor);
+
     playsound(userclickedcolor);
     anim(userclickedcolor);
 });
 
 function nextSequence(){
+    level++;
+    $("h1").text("Level " + level);
+
     var randomnumber = Math.floor(Math.random()*4);
     var randomcolor = buttoncolors[randomnumber];
     gamepattern.push(randomcolor);
+
     $("#"+randomcolor).fadeIn(100).fadeOut(100).fadeIn(100);
     playsound(randomcolor);
-    anim(randomcolor);
+    // anim(randomcolor);
     // console.log(gamepattern);
 }
 
