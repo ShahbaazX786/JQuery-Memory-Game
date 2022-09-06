577953b6fb6c49df79d7c2d656a115e2fe08a35c
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-06 17:11:32 +0530

    CL-1.06-Added functions for userclick sound and animation.

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-gamepattern=[];
-
 buttoncolors = ["red","blue","green","yellow"];
 
-function randum(){
-    var randomnumber=Math.floor(Math.random()*4);
-    var randomcolor=buttoncolors[randomnumber];
+gamepattern=[];
+
+userClickedPattern=[];
+
+
+$(".btn").click(function (){
+    var userclickedcolor=$(this).attr("id");
+    userClickedPattern.push(userclickedcolor);
+    playsound(userclickedcolor);
+    anim(userclickedcolor);
+});
+
+function nextSequence(){
+    var randomnumber = Math.floor(Math.random()*4);
+    var randomcolor = buttoncolors[randomnumber];
     gamepattern.push(randomcolor);
+    $("#"+randomcolor).fadeIn(100).fadeOut(100).fadeIn(100);
     playsound(randomcolor);
-    console.log(gamepattern);
+    anim(randomcolor);
+    // console.log(gamepattern);
 }
-
-// $("#randomcolor")
-
-// // function anime(randomcolor){
-// //     $("#"+randomcolor).fadeIn(100).fadeOut(100);
-// // }
 
 function playsound(randomcolor){
     var sfx = new Audio('sounds/'+randomcolor+".mp3");
     sfx.play();
+}
+
+function anim(color){
+    $("#"+color).addClass("pressed");
+    setTimeout(function(){
+        $("#"+color).removeClass("pressed");
+    },100);
 }
